dd48307eb4811198c9a72f2eb82bc80e7bcb3db9
pozytiv4ik <pozytiv4ik@gmail.com>
2019-11-18 05:21:36 +0600

    Сделал часть задания 3

--- js/filter.js
+++ js/filter.js
@@ -2,24 +2,44 @@
 
 (function () {
   var mapFilters = document.querySelector('.map__filters');
-  var housingTypeFilter = document.querySelector('select[name="housing-type"]');
-  var housingPriceFilter = document.querySelector('select[name="housing-price"]');
-  var housingRoomsNumberFilter = document.querySelector('select[name="housing-rooms"]');
-  var housingGuestsNumberFilter = document.querySelector('select[name="housing-guests"]');
-  var housingFeaturesFilter = document.querySelector('select[name="housing-features"]');
+  var housingTypeFilter = mapFilters.querySelector('select[name="housing-type"]');
+  var housingPriceFilter = mapFilters.querySelector('select[name="housing-price"]');
+  var housingRoomsNumberFilter = mapFilters.querySelector('select[name="housing-rooms"]');
+  var housingGuestsNumberFilter = mapFilters.querySelector('select[name="housing-guests"]');
+  var featureWifi = document.querySelector('#filter-wifi');
+  var featureDishwasher = document.querySelector('#filter-dishwasher');
+  var featureParking = document.querySelector('#filter-parking');
+  var featureWasher = document.querySelector('#filter-washer');
+  var featureElevator = document.querySelector('#filter-elevator');
+  var featureConditioner = document.querySelector('#filter-conditioner');
 
   var cards = [];
   var newCardsbyType = [];
   var newCardsbyPrice = [];
   var newCardsbyRoomsNumber = [];
   var newCardsbyGuestsNumber = [];
+  var newCardsbyFeatures = [];
 
-  var updateRenderbyType = function (type) {
-    if (type === 'any') {
+  var updateRenderbyFeatures = function (featuresType) {
+    if (featuresType === 'any') {
+      newCardsbyFeatures = cards;
+    } else {
+      newCardsbyFeatures = cards.filter(function (it) {
+        var a = it.offer.features;
+        console.log(featuresType.includes(a));
+        return featuresType.includes(a);
+      });
+    }
+    console.log(newCardsbyFeatures);
+    return newCardsbyFeatures;
+  };
+
+  var updateRenderbyType = function (housingType) {
+    if (housingType === 'any') {
       newCardsbyType = cards;
     } else {
       newCardsbyType = cards.filter(function (it) {
-        return it.offer.type === type;
+        return it.offer.type === housingType;
       });
     }
     return newCardsbyType;
@@ -82,13 +102,17 @@
     return newCardsbyGuestsNumber;
   };
 
-  var updateRender = function (renderFunction, type) {
-    updateRenderbyType(type);
+  var updateRender = function (renderFunction, housingType, featuresType) {
+    updateRenderbyType(housingType);
     updateRenderbyPrice();
     updateRenderbyRoomsNumber();
     updateRenderbyGuestsNumber();
+    updateRenderbyFeatures(featuresType);
+    var newCardsLevel4 = newCardsbyGuestsNumber.filter(function (it) {
+      return newCardsbyFeatures.indexOf(it) !== -1;
+    });
     var newCardsLevel3 = newCardsbyRoomsNumber.filter(function (it) {
-      return newCardsbyGuestsNumber.indexOf(it) !== -1;
+      return newCardsLevel4.indexOf(it) !== -1;
     });
     var newCardsLevel2 = newCardsbyPrice.filter(function (it) {
       return newCardsLevel3.indexOf(it) !== -1;
@@ -107,26 +131,56 @@
   };
 
   mapFilters.addEventListener('change', function (evt) {
+    var featuresType = [];
     if (evt.target.id === 'housing-type') {
-      var type = evt.target.value;
+      var housingType = evt.target.value;
     } else {
-      type = housingTypeFilter.value;
+      housingType = housingTypeFilter.value;
     }
+    // if (evt.target.name === 'features') {
+    //   if (evt.target.checked) {
+    //     array += array;
+    //     console.log(array);
+    //     featuresType = array;
+    //   } else {
+    //     featuresType = 'any';
+    //   }
+    // }
+    if (featureWifi.checked) {
+      featuresType.push(featureWifi.value);
+    }
+    if (featureDishwasher.checked) {
+      featuresType.push(featureDishwasher.value);
+    }
+    if (featureParking.checked) {
+      featuresType.push(featureParking.value);
+    }
+    if (featureWasher.checked) {
+      featuresType.push(featureWasher.value);
+    }
+    if (featureElevator.checked) {
+      featuresType.push(featureElevator.value);
+    }
+    if (featureConditioner.checked) {
+      featuresType.push(featureConditioner.value);
+    }
+
+    console.log(featuresType);
     removePreviousRender('.map__card');
     removePreviousRender('.map__pin-advert');
-    updateRender(window.createElements.renderAdvertPinsOnMap, type);
-    updateRender(window.createElements.renderAdvertCardsOnMap, type);
+    updateRender(window.createElements.renderAdvertPinsOnMap, housingType, featuresType);
+    updateRender(window.createElements.renderAdvertCardsOnMap, housingType, featuresType);
     window.manipulateMap.activateMapPins();
   });
 
   var renderFilteredPinsOnMap = function (data) {
     cards = data;
-    updateRender(window.createElements.renderAdvertPinsOnMap, 'any');
+    updateRender(window.createElements.renderAdvertPinsOnMap, 'any', 'any');
   };
 
   var renderFilteredCardsOnMap = function (data) {
     cards = data;
-    updateRender(window.createElements.renderAdvertCardsOnMap, 'any');
+    updateRender(window.createElements.renderAdvertCardsOnMap, 'any', 'any');
   };
 
   var onXhrLoadEnd = function (data) {
