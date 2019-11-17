3b807beab163e609f6c151a7ba3169c3b8b8bea6
pozytiv4ik <pozytiv4ik@gmail.com>
2019-11-17 20:01:11 +0600

    Сделал часть задания 2

--- js/filter.js
+++ js/filter.js
@@ -4,13 +4,15 @@
   var mapFilters = document.querySelector('.map__filters');
   var housingTypeFilter = document.querySelector('select[name="housing-type"]');
   var housingPriceFilter = document.querySelector('select[name="housing-price"]');
-  // var housingRoomsNumberFilter = document.querySelector('select[name="housing-rooms"]');
-  // var housingGuestsNumberFilter = document.querySelector('select[name="housing-guests"]');
-  // var housingFeaturesFilter = document.querySelector('select[name="housing-features"]');
+  var housingRoomsNumberFilter = document.querySelector('select[name="housing-rooms"]');
+  var housingGuestsNumberFilter = document.querySelector('select[name="housing-guests"]');
+  var housingFeaturesFilter = document.querySelector('select[name="housing-features"]');
 
   var cards = [];
   var newCardsbyType = [];
   var newCardsbyPrice = [];
+  var newCardsbyRoomsNumber = [];
+  var newCardsbyGuestsNumber = [];
 
   var updateRenderbyType = function (type) {
     if (type === 'any') {
@@ -42,13 +44,59 @@
     return newCardsbyPrice;
   };
 
+  var updateRenderbyRoomsNumber = function () {
+    if (housingRoomsNumberFilter.selectedIndex === 0) {
+      newCardsbyRoomsNumber = cards;
+    } else if (housingRoomsNumberFilter.selectedIndex === 1) {
+      newCardsbyRoomsNumber = cards.filter(function (it) {
+        return it.offer.rooms === 1;
+      });
+    } else if (housingRoomsNumberFilter.selectedIndex === 2) {
+      newCardsbyRoomsNumber = cards.filter(function (it) {
+        return it.offer.rooms === 2;
+      });
+    } else if (housingRoomsNumberFilter.selectedIndex === 3) {
+      newCardsbyRoomsNumber = cards.filter(function (it) {
+        return it.offer.rooms === 3;
+      });
+    }
+    return newCardsbyRoomsNumber;
+  };
+
+  var updateRenderbyGuestsNumber = function () {
+    if (housingGuestsNumberFilter.selectedIndex === 0) {
+      newCardsbyGuestsNumber = cards;
+    } else if (housingGuestsNumberFilter.selectedIndex === 1) {
+      newCardsbyGuestsNumber = cards.filter(function (it) {
+        return it.offer.guests === 2;
+      });
+    } else if (housingGuestsNumberFilter.selectedIndex === 2) {
+      newCardsbyGuestsNumber = cards.filter(function (it) {
+        return it.offer.guests === 1;
+      });
+    } else if (housingGuestsNumberFilter.selectedIndex === 3) {
+      newCardsbyGuestsNumber = cards.filter(function (it) {
+        return it.offer.guests === 0;
+      });
+    }
+    return newCardsbyGuestsNumber;
+  };
+
   var updateRender = function (renderFunction, type) {
     updateRenderbyType(type);
     updateRenderbyPrice();
-    var newCards = newCardsbyType.filter(function (it) {
-      return newCardsbyPrice.indexOf(it) !== -1;
+    updateRenderbyRoomsNumber();
+    updateRenderbyGuestsNumber();
+    var newCardsLevel3 = newCardsbyRoomsNumber.filter(function (it) {
+      return newCardsbyGuestsNumber.indexOf(it) !== -1;
     });
-    renderFunction(newCards);
+    var newCardsLevel2 = newCardsbyPrice.filter(function (it) {
+      return newCardsLevel3.indexOf(it) !== -1;
+    });
+    var newCardsLevel1 = newCardsbyType.filter(function (it) {
+      return newCardsLevel2.indexOf(it) !== -1;
+    });
+    renderFunction(newCardsLevel1);
   };
 
   var removePreviousRender = function (className) {
